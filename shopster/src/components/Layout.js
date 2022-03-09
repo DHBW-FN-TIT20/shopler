7e5aaaa95e45e67439b68c1e162f56d74e6c1e45
Dominik Klisa <dominik.klisa@outlook.com>
2022-03-09 20:27:08 +0100

    changed folder structure

---
+++ shopster/src/components/Layout.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Shop from "./Shop";
+import SignIn from "./SignIn";
+import NewArticle from "./NewArticle";
+import { Route, Routes } from "react-router-dom";
+import Navigation from "./Navigation";
+import SignUp from "./SignUp";
+import Cart from "./Cart";
+import { useMediaQuery } from "@mui/material";
+
+export default function Layout() {
+  const theme = useTheme();
+
+  const isGreaterThanSmallBreakpoint = useMediaQuery(
+    theme.breakpoints.up("sm")
+  );
+
+  return (
+    <Box>
+      <Navigation smallscreen={isGreaterThanSmallBreakpoint} />
+
+      <Box
+        sx={{
+          marginLeft: isGreaterThanSmallBreakpoint
+            ? `calc(${theme.spacing(7)} + 1px)`
+            : null,
+          marginTop: !isGreaterThanSmallBreakpoint
+            ? `calc(${theme.spacing(7)} + 1px)`
+            : null,
+        }}
+      >
+        <Routes>
+          <Route path="shop" element={<Shop />} />
+          <Route path="newarticle" element={<NewArticle />} />
+          <Route path="cart" element={<Cart />} />
+
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+}
---
+++ shopster/src/components/SignIn.js
@@ -0,0 +1,66 @@
+import { Box, Button, Container, CssBaseline, Grid, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+  return(
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h2">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Username"
+              name="email"
+              autoComplete=""
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Typography>
+              <Link to={{pathname: "/signup"}}>
+                  Don't have an account? Sign Up
+              </Link>
+            </Typography>
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ mt: 2, float: 'right' }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+  )
+}
