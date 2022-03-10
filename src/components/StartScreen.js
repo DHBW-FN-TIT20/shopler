e7e5f94309068d3ae63abb809d09c178d77d1e1c
Nico <nicodahlhaus@outlook.de>
2022-03-10 01:43:36 +0100

    removed blurr finished startscreen open for optimization

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,32 +1,53 @@
 import { Container, Paper, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, createTheme, fontFamily } from "@mui/system";
 import { render } from "@testing-library/react";
 import image from "../img/einkaufen.jpg";
 import { BackdropFilter } from '@mui/material';
 import { sizing } from '@mui/system';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
 
 const styles = {
   Container: {
     backgroundImage: `url(${image})`,
     height: '100vh',
     backgroundSize: 'cover',
-    filter: "blur(2px)"
+    //filter: "blur(2px)"
   },
-  box: {
+  headline: {
     height: '100vh',
+    filter: "none",
     display: 'flex',
-    fontSize: '80px',
+    fontSize: 140,
     alignItems: 'center',
     justifyContent: 'center',
+    fontStyle: 'bold',
+    fontFamily: 'sans-serif',
   }
 };
 
+
 export default function StartScreen(){
   return(
-    <div style={styles.Container}>
-      <Typography style={styles.box}>
+    <Container style={styles.Container} theme={theme}>
+      <Typography style={styles.headline}>
         Shopler
       </Typography>
-    </div>
+    </Container>
   )
 };
