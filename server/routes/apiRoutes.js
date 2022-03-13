df93de20d7d1c54144871ab7180aac2ef7dcd927
Dominik Klisa <dominik.klisa@outlook.com>
2022-03-13 23:44:06 +0100

    finish newItem Route

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -7,32 +7,33 @@ const { Category } = require("./../models/categories");
 const { Item } = require("./../models/items");
 const { database } = require('../bin/db/connect');
 
-router.get("/newitem", verifyUser, async (req, res, next) => {
+router.post("/newitem", verifyUser, async (req, res, next) => {
     const data = await req.body;
     const user = await req.user;
 
-    const categoryIdList = req.body.categoryList;
-    const itemName = req.body.itemName;
-    const itemDescription = req.body.itemDescription;
+    const categoryIdList = data.categoryList;
+    const itemName = data.itemName;
+    const itemDescription = data.itemDescription;
 
     if (!itemName) {
         res.statusCode = 400;
         return res.send("Item Name missing.");
     }
 
-    const {count, categories} = await Category.findAndCountAll({where: {id: categoryIdList}});
-    if (count !== categoryIdList.length) {
+    const categories = await Category.findAll({where: {id: categoryIdList}});
+    if (categories.length !== categoryIdList.length) {
         res.statusCode = 404;
         return res.send("Category not found.");
     }
     try {
         await database.transaction(async (t) => {
-            const item = await Item.create({name: itemName, discription: itemDescription, userId: user.id}, {transaction: t});
+            const item = await Item.create({name: itemName, description: itemDescription, userId: user.id}, {transaction: t});
             categories.forEach(category => {
                 item.addCategory(category);
             });
         });
     } catch (err) {
+        console.log(err);
         res.statusCode = 500;
         return res.send("Internal Server Error");
     }
