240ae39ee2bc51946ff076d28172fec45bd91e4e
Nico <nicodahlhaus@outlook.de>
2022-03-13 02:19:40 +0100

    every paper has same size

--- frontend/src/pages/Privacy.js
+++ frontend/src/pages/Privacy.js
@@ -11,7 +11,6 @@ const styles = {
     backgroundSize: "cover",
     backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
-    backgroundAttatchment: "fixed",
   },
   Logo:{
     backgroundRepeat: "no-repeat",
@@ -27,9 +26,7 @@ const styles = {
 export default function StartScreen() {
   return (
     <Container>
-      <view>
-        <Container style={styles.Container} className="FirstView"> </Container>
-      </view>
+        <Container style={styles.Container} className="FirstView"> 
         <Container maxWidth="55%" component={Paper} variant={"outlined"} style={styles.TextField}>
           <Typography textAlign={"center"} alignContent={"center"} height={"35vh"} width={"20%"} variant="h2">
             <img src={Logo} alt="logo" width={"500%"}/>
@@ -76,5 +73,6 @@ export default function StartScreen() {
           </Typography>
         </Container>
       </Container>
+    </Container>
   );
 }
