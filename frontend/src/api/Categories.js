cc2fa4500f15a23c36b969fc50187c40f876b2db
Dominik Klisa <dominik.klisa@outlook.com>
2022-03-14 02:00:44 +0100

    implementing fetch categories

---
+++ frontend/src/api/Categories.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+export async function getCategories(token) {
+    return await fetch(process.env.REACT_APP_API_ENDPOINT + "api/categories", {
+        method: "GET",
+        headers: {
+            "Contenttype": "application/json",
+            Authorization: `Bearer ${token}`
+        }
+    }).then(async response => {
+        if (!response.ok) {
+            console.log(response);
+            return null;
+        }
+        const data = await response.json();
+        return data.categories;
+    }).catch(error => {
+        console.log(error);
+        return null;
+    });
+}
