c53c5aa2dddeb155d1ac0a32506833a1f22c6b88
Dominik Klisa <dominik.klisa@outlook.com>
2022-11-24 16:59:03 +0100

    add sqlite db connector for debugging

---
+++ server/bin/db/connect.mariadb.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+
+/**
+ * Initialize the MariaDB database.
+ */
+const database = new Sequelize({
+    host: process.env.DB_HOST,
+    dialect: 'mariadb',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+
+exports.database = database;
